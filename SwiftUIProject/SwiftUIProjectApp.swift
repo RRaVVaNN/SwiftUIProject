483a17e77540a872d24a44f06305466c2e7df11b
Ravan Asgarov <ravanasgarov@icloud.com>
2026-01-04 23:11:31 +0400

    initial commit

---
+++ SwiftUIProject/SwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectApp.swift
+//  SwiftUIProject
+//
+//  Created by Ravan Asgarov on 04.01.26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
